c53bb7dcbd82e954accef54390c76f984ea97f97
Éricles Miller <ericlesmiller15@gmail.com>
2025-02-10 00:19:49 -0300

    feat: create a new movie and find all movies in favorite list

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, UseGuards, Req } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, UseGuards, Req, Query } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
 import { AuthGuard } from 'src/accounts/auth/AuthGuards';
-import { ApiBearerAuth } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiQuery } from '@nestjs/swagger';
 import { RequestWithUser } from 'src/types/request-with-user';
+import { PaginatedListDto } from './dto/paginated-list.dto';
+import { Movie } from './entities/movie.entity';
 
 @Controller('movies')
 export class MoviesController {
@@ -18,9 +20,27 @@ export class MoviesController {
     return await this.moviesService.create(createMovieDto, user.accountId, user.sessionId);
   }
 
-  @Get()
-  findAll() {
-    return this.moviesService.findAll();
+  @Get('database')
+  @ApiBearerAuth('sessionAuth')
+  @UseGuards(AuthGuard)
+  @ApiQuery({ name: 'page', required: false, type: Number })
+  @ApiQuery({ name: 'limit', required: false, type: Number })
+  async findAllDatabase(
+    @Query('page') page?: string,
+    @Query('limit') limit?: string,
+  ): Promise<PaginatedListDto<Movie[]>> {
+    const pageNumber = Number(page) > 0 ? Number(page) : 1;
+    const limitNumber = Number(limit) > 0 ? Number(limit) : 10;
+
+    return await this.moviesService.findAllDatabase(pageNumber, limitNumber);
+  }
+
+  @Get('api')
+  @Get('database')
+  @ApiBearerAuth('sessionAuth')
+  async findAllAPI(@Req() request: RequestWithUser) {
+    const user = request.user;
+    return await this.moviesService.findAllApi(user.accountId, user.sessionId);
   }
 
   @Get(':id')
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -2,10 +2,20 @@ import { BadRequestException, Injectable, InternalServerErrorException } from '@
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
 import { HttpService } from '@nestjs/axios';
+import { Movie } from './entities/movie.entity';
+import { MovieDto } from './dto/movie.dto';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PaginatedListDto } from './dto/paginated-list.dto';
 
 @Injectable()
 export class MoviesService {
-  constructor(private readonly httpService: HttpService) {}
+  constructor(
+    private readonly httpService: HttpService,
+
+    @InjectRepository(Movie)
+    private readonly repository: Repository<Movie>,
+  ) {}
 
   async create({ name }: CreateMovieDto, accountId: number, sessionId: string): Promise<void> {
     const findMovieByName = await this.httpService.axiosRef.get(
@@ -22,16 +32,81 @@ export class MoviesService {
       throw new BadRequestException('Movie name not found');
     }
 
-    const movie = findMovieByName.data.results.find((movie) => movie.title === name);
+    const movie: MovieDto = findMovieByName.data.results.find((movie) => movie.title === name);
+
     if (!movie) {
       throw new BadRequestException('Movie name not found');
     }
 
-    await this.addFavorite(accountId, sessionId, movie.id.toString());
+    const {
+      adult,
+      backdrop_path,
+      id,
+      original_language,
+      original_title,
+      overview,
+      popularity,
+      poster_path,
+      release_date,
+      title,
+      vote_average,
+      vote_count,
+    } = movie;
+
+    const movieEntity = new Movie(
+      adult,
+      backdrop_path,
+      id,
+      original_language,
+      original_title,
+      overview,
+      popularity,
+      poster_path,
+      release_date,
+      title,
+      vote_average,
+      vote_count,
+    );
+
+    await this.addFavoriteListAPI(accountId, sessionId, movie.id.toString());
+    await this.repository.save(movieEntity);
   }
 
-  findAll() {
-    return `This action returns all movies`;
+  async findAllDatabase(page: number, limit: number): Promise<PaginatedListDto<Movie[]>> {
+    const [movies, total] = await this.repository.findAndCount({
+      take: limit,
+      skip: (page - 1) * limit,
+    });
+
+    return {
+      data: movies,
+      total,
+      page,
+      lastPage: Math.ceil(total / limit),
+    };
+  }
+
+  async findAllApi(accountId: number, sessionId: string): Promise<PaginatedListDto<any[]>> {
+    try {
+      const response = await this.httpService.axiosRef.get(
+        `https://api.themoviedb.org/3/account/${accountId}/favorite/movies`,
+        {
+          params: {
+            api_key: process.env.API_KEY,
+            session_id: sessionId,
+          },
+        },
+      );
+
+      return {
+        data: response.data.results,
+        total: response.data.total_results,
+        page: response.data.page,
+        lastPage: response.data.total_pages,
+      };
+    } catch (error) {
+      throw new InternalServerErrorException('Erro ao listar filmes favoritos', error);
+    }
   }
 
   findOne(id: number) {
@@ -42,7 +117,7 @@ export class MoviesService {
     return `This action updates a #${id} movie`;
   }
 
-  async addFavorite(accountId: number, sessionId: string, movieId: string): Promise<void> {
+  async addFavoriteListAPI(accountId: number, sessionId: string, movieId: string): Promise<void> {
     try {
       await this.httpService.axiosRef.post(
         `https://api.themoviedb.org/3/account/${accountId}/favorite`,
